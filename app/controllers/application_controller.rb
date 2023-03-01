6f6a94e6a68dac300ea97ce6df1e80c3199f1f76
Collins Chirchir <collinskchirchir@gmail.com>
2023-03-01 19:50:39 +0300

    Done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
 
@@ -14,6 +15,46 @@ class ApplicationController < Sinatra::Base
         user: { only: [:name] }
       } }
     })
+  end
+
+  get '/reviews' do
+    # find the review using the id
+    reviews = Review.all
+    reviews.to_json
+  end
+
+  get '/reviews/:id' do
+    # find the review using the id
+    review = Review.find(params[:id])
+    review.to_json
+  end
+
+  delete '/reviews/:id' do
+    # find the review using the id
+    review = Review.find(params[:id])
+    #delete the review
+    review.destroy
+    # send a response with the deleted review as JSON
+    review.to_json
+  end
+
+  post '/reviews' do
+    review = Review.create(
+      score: params[:score],
+      comment: params[:comment],
+      game_id: params[:game_id],
+      user_id: params[:user_id]
+    )
+    review.to_json
+  end
+
+  patch '/reviews/:id' do
+    review = Review.find(params[:id])
+    review.update(
+      score: params[:score],
+      comment: params[:comment]
+    )
+    review.to_json
   end
 
 end
